cddcb3695a68fe9840c4aa6b77602ef9d387b30e
brunno  henrique <bhsilva5807@gmail.com>
2022-04-27 18:26:25 -0300

    Initial commit

---
+++ exercicios/ex021/ambient.js
@@ -0,0 +1,7 @@
+function clicou(){
+window.alert('meus parabens voce clicou no botao inutil')
+}
+function bnome(){
+    let nome = window.prompt('qual é o seu nome?')
+    window.alert (`Muito prazer, ${nome}`)
+}
---
+++ exercicios/ex021/login.js
@@ -0,0 +1,8 @@
+function login(){
+    let nomeregistro = document.getElementById('fname')
+    let usuario = nomeregistro
+    if(usuario != nomeregistro){
+        alert('nome não registrado.')
+    }
+
+}
